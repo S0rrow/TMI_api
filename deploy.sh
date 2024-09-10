9e0a2b093724abcf13a9a4c3e35ace55173946b5
S0rrow <jae9hell@gmail.com>
2024-09-10 14:55:26 +0900

    [ADD] workflow added

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y python3-pip python3-venv
+
+cd /home/ubuntu/TMI_api
+
+# virtual environemnt variable
+VENV_DIR="venv"
+
+# check if venv already exists
+if [ -d "$VENV_DIR"]; then
+  echo "Virtual Environment already exists. Updating dependencies..."
+  source $VENV_DIR/bin/activate
+else
+  echo "Virtual environment does not exist. Creating a new one..."
+  python3 -m venv $VENV_DIR
+  source $VENV_DIR/bin/activate
+fi
+
+# install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# kill running api
+if lsof -i :8000; then
+  pkill -f 'uvicorn app:app'
+fi
+
+nohup uvicorn -w 4 -b 0.0.0.0:8000 app:app --reload > fastapi.log 2>&1 &
+
+echo "api deployment complete."
